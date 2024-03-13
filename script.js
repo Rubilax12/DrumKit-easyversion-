f0d1e63c4f43574be9e6b70cdb04e3f49d4fce6b
MambaLite <145127257+Rubilax12@users.noreply.github.com>
2024-03-13 21:52:59 +0100

    Version 1.0
    (french drumkit)

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function play(){
+    console.log("Le son 1 est joué");
+    var audio = new Audio('song1.mp3');
+    audio.play();
+}
+
+function play2(){
+    console.log("Le son 2 est joué");
+    var audio = new Audio('song2.mp3');
+    audio.play();
+}
+
+function play3(){
+    console.log("Le son 3 est joué");
+    var audio = new Audio('song3.mp3');
+    audio.play();
+}
